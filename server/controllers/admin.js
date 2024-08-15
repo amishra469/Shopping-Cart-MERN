011c9392b204f9f9c766286840bf662f03d3a76f
amishra469 <svmaditya01@gmail.com>
2024-08-15 23:24:57 +0530

    Added Cart Logic

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -10,13 +10,28 @@ exports.getAddProduct = (req, res, next) => {
 // Endpoint to handle adding a product
 exports.postAddProduct = (req, res, next) => {
     const { title, imageUrl, price, description } = req.body;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.status(201).json({
         message: 'Product added successfully',
         product: product
     });
 };
+
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.prodId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDesc = req.body.description;
+
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+    updatedProduct.save();
+    res.status(201).json({
+        message: 'Product Updated successfully',
+        product: updatedProduct
+    });
+}
 
 // Endpoint to fetch all products
 exports.getProducts = (req, res, next) => {
